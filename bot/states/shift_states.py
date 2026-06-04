13a4d9f2b4f197f3ecbc20b67d2fb19e5982d7c8
syncae <syncaeoff@gmail.com>
2026-06-04 20:15:45 +0200

    Add files via upload

--- bot/states/shift_states.py
+++ bot/states/shift_states.py
@@ -1,1 +1,10 @@
+from aiogram.fsm.state import State, StatesGroup
 
+
+class RateStates(StatesGroup):
+    waiting_for_rate = State()
+
+
+class ManualShiftStates(StatesGroup):
+    waiting_for_date = State()
+    waiting_for_hours = State()
